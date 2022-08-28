aa987980fe1441cd19d378241559787e2d088421
mishaDrigval <drigval.ma.11@gmail.com>
2022-08-28 15:39:18 +0300

    fix lint

--- src/js/gameSavingLoader.js
+++ src/js/gameSavingLoader.js
@@ -3,18 +3,16 @@ import read from './reader';
 
 export default class GameSavingLoader {
   static load() {
-    // eslint-disable-next-line no-async-promise-executor
-    return new Promise(async (resolve, reject) => {
-        const data =  read();
-        data.then((result) =>{
-          const GameSaving =  json(result);
-          return GameSaving
-        }).then((GameSaving) => {
-          resolve({GameSaving});
-        }).catch(error =>{
-          throw new Error (error);
-        })
+    return new Promise((resolve) => {
+      const data = read();
+      data.then((result) => {
+        const GameSaving = json(result);
+        return GameSaving;
+      }).then((GameSaving) => {
+        resolve({ GameSaving });
+      }).catch((error) => {
+        throw new Error(error);
+      });
     });
   }
 }
-
